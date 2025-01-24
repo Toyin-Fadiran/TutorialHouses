efe46ad41de806ae8f0ed8743b1e6ba77bc7b31b
Toyin-Fadiran <toyin.fadiran@gmail.com>
2025-01-23 22:40:31 -0600

    up to angular directives

--- src/app/housing-location/housing-location.component.ts
+++ src/app/housing-location/housing-location.component.ts
@@ -1,11 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { HousingLocation } from '../housinglocation';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-housing-location',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './housing-location.component.html',
   styleUrls: ['./housing-location.component.css'],
 })
 export class HousingLocationComponent {
+
+  //field or property
+  @Input() housingLocation!: HousingLocation;
 
 }
